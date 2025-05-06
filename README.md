993e3400e95147892b023c734d0b286c32b2b48d
cvrain <cvraindays@outlook.com>
2025-05-06 21:18:20 +0800

    Hello world!让项目可以在windows和linux上运行

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lower house vote log
+## The record of the House of Commons defeating the House of Lords
+
+### How to use
+1. Clone the repo  
+    ```bash 
+    git clone https://github.com/CvRain/LowerHouseVoteLog.git --recursive
+    ```
+2. Build
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,6 @@
 int main(int argc, char *argv[]) {
     const QGuiApplication app(argc, argv);
 
-    SQLite::Database db("test.db3");
-    SQLite::Statement query(db, "SELECT * FROM test");
-
-
     QQmlApplicationEngine engine;
     QObject::connect(
         &engine,
