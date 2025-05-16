bad361aeb8a96c70a8dc6178c6d19203fa8f6dd3
cvrain <cvraindays@outlook.com>
2025-05-16 12:38:18 +0800

    complete ThemeManager

--- src/theme/theme_manager.cpp
+++ src/theme/theme_manager.cpp
@@ -8,6 +8,12 @@
 
 #include "theme/palette_manager.hpp"
 
+QColor setColorAlpha(const QColor& color, const int alpha) {
+    auto newColor = color;
+    newColor.setAlpha(alpha);
+    return newColor;
+}
+
 ThemeManager* ThemeManager::instance = nullptr;
 
 ThemeManager* ThemeManager::getInstance() {
@@ -25,6 +31,60 @@ ThemeManager::ThemeManager(QObject* parent) {
 }
 
 QColor ThemeManager::getBackgroundPanel() { return PaletteManager::getInstance()->GetBase(); }
+QColor ThemeManager::getSecondaryPanel0() { return PaletteManager::getInstance()->GetCrust(); }
+QColor ThemeManager::getSecondaryPanel1() { return PaletteManager::getInstance()->GetMantle(); }
+QColor ThemeManager::getSurfaceElement0() { return PaletteManager::getInstance()->GetSurface0(); }
+QColor ThemeManager::getSurfaceElement1() { return PaletteManager::getInstance()->GetSurface1(); }
+QColor ThemeManager::getSurfaceElement2() { return PaletteManager::getInstance()->GetSurface2(); }
+QColor ThemeManager::getOverlay0() { return PaletteManager::getInstance()->GetOverlay0(); }
+QColor ThemeManager::getOverlay1() { return PaletteManager::getInstance()->GetOverlay1(); }
+QColor ThemeManager::getOverlay2() { return PaletteManager::getInstance()->GetOverlay2(); }
+QColor ThemeManager::getBodyCopy() { return PaletteManager::getInstance()->GetText(); }
+QColor ThemeManager::getMainHeadline() { return PaletteManager::getInstance()->GetText(); }
+QColor ThemeManager::getLabel0() { return PaletteManager::getInstance()->GetSubtext0(); }
+QColor ThemeManager::getLabel1() { return PaletteManager::getInstance()->GetSubtext1(); }
+QColor ThemeManager::getSubHeadline0() { return PaletteManager::getInstance()->GetSubtext0(); }
+QColor ThemeManager::getSubHeadline1() { return PaletteManager::getInstance()->GetSubtext1(); }
+QColor ThemeManager::getSubtle() { return PaletteManager::getInstance()->GetOverlay1(); }
+QColor ThemeManager::getLink() { return PaletteManager::getInstance()->GetBlue(); }
+QColor ThemeManager::getSuccess() { return PaletteManager::getInstance()->GetGreen(); }
+QColor ThemeManager::getWarning() { return PaletteManager::getInstance()->GetYellow(); }
+QColor ThemeManager::getError() { return PaletteManager::getInstance()->GetRed(); }
+QColor ThemeManager::getTags() { return PaletteManager::getInstance()->GetBlue(); }
+QColor ThemeManager::getSelectionBackground() { return setColorAlpha(PaletteManager::getInstance()->GetBlue(), 33); }
+QColor ThemeManager::getCursor() { return PaletteManager::getInstance()->GetRosewater(); }
+QColor ThemeManager::getCursorText() {
+    return PaletteManager::getInstance()->getCurrentPaletteType() == PaletteManager::ThemeType::Latte
+                   ? PaletteManager::getInstance()->GetBase()
+                   : PaletteManager::getInstance()->GetCrust();
+}
+QColor ThemeManager::getActiveBorder() {return PaletteManager::getInstance()->GetLavender();}
+QColor ThemeManager::getInactiveBorder() { return PaletteManager::getInstance()->GetOverlay0(); }
+QColor ThemeManager::getBellBorder() { return PaletteManager::getInstance()->GetYellow(); }
+QColor ThemeManager::getColor0() {return PaletteManager::getInstance()->GetSubtext1();}
+QColor ThemeManager::getColor1() {return PaletteManager::getInstance()->GetRed();}
+QColor ThemeManager::getColor2() {return PaletteManager::getInstance()->GetGreen();}
+QColor ThemeManager::getColor3() {return PaletteManager::getInstance()->GetYellow();}
+QColor ThemeManager::getColor4() {return PaletteManager::getInstance()->GetBlue();}
+QColor ThemeManager::getColor5() {return PaletteManager::getInstance()->GetPink();}
+QColor ThemeManager::getColor6() {return PaletteManager::getInstance()->GetTeal();}
+QColor ThemeManager::getColor7() {return PaletteManager::getInstance()->GetSurface2();}
+QColor ThemeManager::getColor8() {return PaletteManager::getInstance()->GetSubtext0();}
+QColor ThemeManager::getColor9() {return PaletteManager::getInstance()->GetRed().lighter();}
+QColor ThemeManager::getColor10() {return PaletteManager::getInstance()->GetGreen().lighter();}
+QColor ThemeManager::getColor11() {return PaletteManager::getInstance()->GetYellow().lighter();}
+QColor ThemeManager::getColor12() {return PaletteManager::getInstance()->GetBlue().lighter();}
+QColor ThemeManager::getColor13() {return PaletteManager::getInstance()->GetPink().lighter();}
+QColor ThemeManager::getColor14() {return PaletteManager::getInstance()->GetTeal().lighter();}
+QColor ThemeManager::getColor15() {return PaletteManager::getInstance()->GetSurface1();}
+QColor ThemeManager::getColor16() {return PaletteManager::getInstance()->GetPeach();}
+QColor ThemeManager::getColor17() {return PaletteManager::getInstance()->GetRosewater();}
+QColor ThemeManager::getMark1() {return PaletteManager::getInstance()->GetLavender();}
+QColor ThemeManager::getMark1Text() {return PaletteManager::getInstance()->GetBase();}
+QColor ThemeManager::getMark2() {return PaletteManager::getInstance()->GetMauve();}
+QColor ThemeManager::getMark2Text() {return PaletteManager::getInstance()->GetBase();}
+QColor ThemeManager::getMark3() {return PaletteManager::getInstance()->GetSapphire();}
+QColor ThemeManager::getMark3Text() {return PaletteManager::getInstance()->GetBase();}
 
 void ThemeManager::themeChangedMessage() {
     qDebug() << "ThemeManager::themeChangedMessage: " << PaletteManager::getInstance()->getCurrentPaletteText();
--- src/theme/theme_manager.hpp
+++ src/theme/theme_manager.hpp
@@ -9,9 +9,7 @@
 #include <QObject>
 #include <QQmlEngine>
 #include <QString>
-#include <map>
 
-#include "palette.hpp"
 #include "palette_manager.hpp"
 
 class ThemeManager : public QObject {
@@ -19,12 +17,112 @@ class ThemeManager : public QObject {
     QML_ELEMENT
     QML_SINGLETON
     Q_PROPERTY(QColor backgroundPanel READ getBackgroundPanel NOTIFY themeChanged)
+    Q_PROPERTY(QColor secondaryPanel0 READ getSecondaryPanel0 NOTIFY themeChanged)
+    Q_PROPERTY(QColor secondaryPanel1 READ getSecondaryPanel1 NOTIFY themeChanged)
+    Q_PROPERTY(QColor surfaceElement0 READ getSurfaceElement0 NOTIFY themeChanged)
+    Q_PROPERTY(QColor surfaceElement1 READ getSurfaceElement1 NOTIFY themeChanged)
+    Q_PROPERTY(QColor surfaceElement2 READ getSurfaceElement2 NOTIFY themeChanged)
+    Q_PROPERTY(QColor overlay0 READ getOverlay0 NOTIFY themeChanged)
+    Q_PROPERTY(QColor overlay1 READ getOverlay1 NOTIFY themeChanged)
+    Q_PROPERTY(QColor overlay2 READ getOverlay2 NOTIFY themeChanged)
+    Q_PROPERTY(QColor bodyCopy READ getBodyCopy NOTIFY themeChanged)
+    Q_PROPERTY(QColor mainHeadline READ getMainHeadline NOTIFY themeChanged)
+    Q_PROPERTY(QColor label0 READ getLabel0 NOTIFY themeChanged)
+    Q_PROPERTY(QColor label1 READ getLabel1 NOTIFY themeChanged)
+    Q_PROPERTY(QColor subHeadline0 READ getSubHeadline0 NOTIFY themeChanged)
+    Q_PROPERTY(QColor subHeadline1 READ getSubHeadline1 NOTIFY themeChanged)
+    Q_PROPERTY(QColor subtle READ getSubtle NOTIFY themeChanged)
+    Q_PROPERTY(QColor link READ getLink NOTIFY themeChanged)
+    Q_PROPERTY(QColor success READ getSuccess NOTIFY themeChanged)
+    Q_PROPERTY(QColor warning READ getWarning NOTIFY themeChanged)
+    Q_PROPERTY(QColor error READ getError NOTIFY themeChanged)
+    Q_PROPERTY(QColor tags READ getTags NOTIFY themeChanged)
+    Q_PROPERTY(QColor selectionBackground READ getSelectionBackground NOTIFY themeChanged)
+    Q_PROPERTY(QColor cursor READ getCursor NOTIFY themeChanged)
+    Q_PROPERTY(QColor cursorText READ getCursorText NOTIFY themeChanged)
+    Q_PROPERTY(QColor activeBorder READ getActiveBorder NOTIFY themeChanged)
+    Q_PROPERTY(QColor inactiveBorder READ getInactiveBorder NOTIFY themeChanged)
+    Q_PROPERTY(QColor bellBorder READ getBellBorder NOTIFY themeChanged)
+    Q_PROPERTY(QColor color0 READ getColor0 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color1 READ getColor1 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color2 READ getColor2 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color3 READ getColor3 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color4 READ getColor4 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color5 READ getColor5 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color6 READ getColor6 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color7 READ getColor7 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color8 READ getColor8 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color9 READ getColor9 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color10 READ getColor10 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color11 READ getColor11 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color12 READ getColor12 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color13 READ getColor13 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color14 READ getColor14 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color15 READ getColor15 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color16 READ getColor16 NOTIFY themeChanged)
+    Q_PROPERTY(QColor color17 READ getColor17 NOTIFY themeChanged)
+    Q_PROPERTY(QColor mark1 READ getMark1 NOTIFY themeChanged)
+    Q_PROPERTY(QColor mark1Text READ getMark1Text NOTIFY themeChanged)
+    Q_PROPERTY(QColor mark2 READ getMark2 NOTIFY themeChanged)
+    Q_PROPERTY(QColor mark2Text READ getMark2Text NOTIFY themeChanged)
+    Q_PROPERTY(QColor mark3 READ getMark3 NOTIFY themeChanged)
+    Q_PROPERTY(QColor mark3Text READ getMark3Text NOTIFY themeChanged)
 
 public:
     static ThemeManager *getInstance();
     static ThemeManager *create(QQmlEngine *, QJSEngine *);
 
     static QColor getBackgroundPanel();
+    static QColor getSecondaryPanel0();
+    static QColor getSecondaryPanel1();
+    static QColor getSurfaceElement0();
+    static QColor getSurfaceElement1();
+    static QColor getSurfaceElement2();
+    static QColor getOverlay0();
+    static QColor getOverlay1();
+    static QColor getOverlay2();
+    static QColor getBodyCopy();
+    static QColor getMainHeadline();
+    static QColor getLabel0();
+    static QColor getLabel1();
+    static QColor getSubHeadline0();
+    static QColor getSubHeadline1();
+    static QColor getSubtle();
+    static QColor getLink();
+    static QColor getSuccess();
+    static QColor getWarning();
+    static QColor getError();
+    static QColor getTags();
+    static QColor getSelectionBackground();
+    static QColor getCursor();
+    static QColor getCursorText();
+    static QColor getActiveBorder();
+    static QColor getInactiveBorder();
+    static QColor getBellBorder();
+    static QColor getColor0();
+    static QColor getColor1();
+    static QColor getColor2();
+    static QColor getColor3();
+    static QColor getColor4();
+    static QColor getColor5();
+    static QColor getColor6();
+    static QColor getColor7();
+    static QColor getColor8();
+    static QColor getColor9();
+    static QColor getColor10();
+    static QColor getColor11();
+    static QColor getColor12();
+    static QColor getColor13();
+    static QColor getColor14();
+    static QColor getColor15();
+    static QColor getColor16();
+    static QColor getColor17();
+    static QColor getMark1();
+    static QColor getMark1Text();
+    static QColor getMark2();
+    static QColor getMark2Text();
+    static QColor getMark3();
+    static QColor getMark3Text();
 
 public slots:
     void themeChangedMessage();
